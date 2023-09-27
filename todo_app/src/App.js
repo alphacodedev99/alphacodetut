340a92e028bc8d4287024ff10ad6791137d38d50
alphacodedev99 <kiza.milenkovic@gmail.com>
2023-09-27 19:04:29 +0200

    Finish todo app

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import TodoInput from './components/TodoInput/TodoInput';
+import TodoTask from './components/TodoTask/TodoTask';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todoItems, setTodoItems] = useState([]);
+
+	useEffect(() => {
+		console.log(todoItems);
+	}, [todoItems]);
+
+	return (
+		<div className='container'>
+			<h1 className='title'>TODO APP</h1>
+
+			<TodoInput todoItems={todoItems} setTodoItems={setTodoItems} />
+			<TodoTask todoItems={todoItems} setTodoItems={setTodoItems} />
+		</div>
+	);
 }
 
 export default App;
